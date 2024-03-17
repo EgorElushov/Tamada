c3e521db12a3336f2a8c3032faa800b1ba26e3d2
Egor Elushov <egorelusov@gmail.com>
2024-03-17 18:15:29 +0300

    Initial Commit

---
+++ Tamada/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamada
+//
+//  Created by Егор Елушов on 17.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
